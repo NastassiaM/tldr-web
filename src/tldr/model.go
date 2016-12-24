c34135da7742f9e77ace825fef400e35baf58acd
NastassiaM <nastassia.mitskevich@gmail.com>
2016-12-25 01:16:38 +0300

    Comments and json tags are added to model.

--- src/tldr/model.go
+++ src/tldr/model.go
@@ -1,15 +1,19 @@
 package tldr
 
+// Example represents a single example of command line tool usage
+// along with some short description.
 type Example struct {
-	ID          uint
-	CmdLine     string
-	Description string
+	ID          uint   `json:"id"`
+	CmdLine     string `json:"command_line"`
+	Description string `json:"desc"`
 }
 
+// Page represents a single tl;dr-page for a command line tool.
 type Page struct {
-	Name        string
-	Description string
-	Examples    []Example
+	Name        string    `json:"name"`
+	Description string    `json:"desc"`
+	Examples    []Example `json:"examples"`
 }
 
+// Pages is just a collection of Page.
 type Pages []Page
